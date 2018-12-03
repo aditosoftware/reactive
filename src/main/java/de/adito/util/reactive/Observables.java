2347abc5ccea88ba36cca7aebbe642e432c57e86
Werner Glanzer <w.glanzer@adito.de>
2018-12-03 13:52:21 +0100

    "Observables" to create observable instances with default values

---
+++ src/main/java/de/adito/util/reactive/Observables.java
@@ -0,0 +1,50 @@
+package de.adito.util.reactive;
+
+import io.reactivex.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+/**
+ * @author w.glanzer, 03.12.2018
+ */
+public class Observables
+{
+
+  private Observables()
+  {
+  }
+
+  /**
+   * Create an Observable with a default value
+   *
+   * @param pObservableBlueprint Observable to build
+   * @param pDefaultValue        Supplier for the default value. <b>It must not return null!</b>.
+   *                             It will be called if a new observer subscribes
+   * @return the created observable
+   */
+  @NotNull
+  public static <T> Observable<T> create(@NotNull ObservableOnSubscribe<T> pObservableBlueprint, @NotNull Supplier<T> pDefaultValue)
+  {
+    // Get StackTrace to increase readability of the exception
+    Exception stackTrace = new Exception();
+
+    // construct observable
+    return Observable.create(pObservableBlueprint)
+        .startWith(_emitValue(pDefaultValue, stackTrace));
+  }
+
+  @NotNull
+  private static <T> ObservableSource<T> _emitValue(@NotNull Supplier<T> pSupplier, @NotNull Exception pStackTrace)
+  {
+    return Observable.just(pSupplier)
+        .map(pSup -> {
+          T value = pSup.get();
+          if (value == null)
+            throw new RuntimeException("The supplier for the default value must not return null! " +
+                                           "See the cause-trace for more information about the 'create()'-call in which the given supplier returned null.", pStackTrace);
+          return value;
+        });
+  }
+
+}
---
+++ src/test/java/de/adito/util/reactive/Test_Observables.java
@@ -0,0 +1,34 @@
+package de.adito.util.reactive;
+
+import io.reactivex.Observable;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author w.glanzer, 03.12.2018
+ */
+public class Test_Observables
+{
+
+  @Test
+  void test_create()
+  {
+    AtomicBoolean created = new AtomicBoolean(false);
+    Observable<String> observable = Observables.create(emitter -> emitter.onNext("subscribed"), () -> {
+      // This must be called after our chain is created
+      Assertions.assertTrue(created.get());
+      return "default";
+    });
+
+    // Now set the created-flag to TRUE, because we created our observable-chain successfully
+    created.set(true);
+
+    // read all values and assert the correct order
+    List<String> values = new ArrayList<>();
+    observable.subscribe(values::add).dispose();
+    Assertions.assertEquals(Arrays.asList("default", "subscribed"), values);
+  }
+
+}
