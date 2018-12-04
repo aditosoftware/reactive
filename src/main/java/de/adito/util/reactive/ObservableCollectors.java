afda05f37b22d89526b8b07f6ffe7d89338e3c0e
Werner Glanzer <w.glanzer@adito.de>
2018-12-04 13:56:42 +0100

    "ObservableCollectors" that contains Collectors especially for observables

---
+++ src/main/java/de/adito/util/reactive/ObservableCollectors.java
@@ -0,0 +1,93 @@
+package de.adito.util.reactive;
+
+import io.reactivex.Observable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collector;
+
+/**
+ * @author w.glanzer, 04.12.2018
+ */
+public class ObservableCollectors
+{
+
+  private ObservableCollectors()
+  {
+  }
+
+  /**
+   * Returns a collector which combines an Stream of Observables to one observable.
+   * It is combined via Observable.combineLatest()
+   *
+   * @return the Collector
+   */
+  @NotNull
+  public static <T> Collector<Observable<T>, ?, Observable<List<T>>> combineToList()
+  {
+    return _combine(pData -> {
+      List<T> values = new ArrayList<>();
+      for (Object data : pData)
+        values.add((T) data);
+      return values;
+    });
+  }
+
+  /**
+   * Returns a collector which combines an Stream of Observables to one observable.
+   * It is combined via Observable.combineLatest()
+   *
+   * @return the Collector
+   */
+  @NotNull
+  public static <T> Collector<Observable<Optional<T>>, ?, Observable<List<T>>> combineOptionalsToList()
+  {
+    return _combine(pData -> {
+      List<T> values = new ArrayList<>();
+      for (Object data : pData)
+        ((Optional<T>) data).ifPresent(values::add);
+      return values;
+    });
+  }
+
+  /**
+   * Returns a collector which combines an Stream of Observables to one observable.
+   * It is combined via Observable.combineLatest()
+   *
+   * @return the Collector
+   */
+  @NotNull
+  public static <T> Collector<Observable<List<T>>, ?, Observable<List<T>>> combineListToList()
+  {
+    return _combine(pData -> {
+      List<T> values = new ArrayList<>();
+      for (Object data : pData)
+        values.addAll(((List<T>) data));
+      return values;
+    });
+  }
+
+  /**
+   * Returns an Collector which combines Observables to one Observable with a value-List
+   *
+   * @param pFn   Function, which combines the Input-Values to a List of output-Values
+   * @param <IN>  INPUT-Value-Type
+   * @param <OUT> OUTPUT-Value-Type
+   * @return the Collector, not <tt>null</tt>
+   */
+  @NotNull
+  private static <IN, OUT> Collector<Observable<IN>, ?, Observable<List<OUT>>> _combine(@NotNull Function<Object[], List<OUT>> pFn)
+  {
+    return Collector.of((Supplier<ArrayList<Observable<IN>>>) ArrayList::new, ArrayList::add, (pList1, pList2) -> {
+      ArrayList<Observable<IN>> copy = new ArrayList<>(pList1);
+      copy.addAll(pList2);
+      return copy;
+    }, pList -> {
+      if (pList.isEmpty())
+        return Observable.just(Collections.emptyList());
+      return Observable.combineLatest(pList, pFn::apply);
+    });
+  }
+
+}
---
+++ src/test/java/de/adito/util/reactive/Test_ObservableCollectors.java
@@ -0,0 +1,57 @@
+package de.adito.util.reactive;
+
+import io.reactivex.Observable;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+/**
+ * @author w.glanzer, 07.12.2018
+ */
+public class Test_ObservableCollectors
+{
+
+  @Test
+  void test_combineToList()
+  {
+    Observable<List<Integer>> observableList = Stream.of(Observable.just(1), Observable.just(2), Observable.just(3), Observable.just(4))
+        .collect(ObservableCollectors.combineToList());
+
+    AtomicInteger callCounter = new AtomicInteger(0);
+    observableList.subscribe(pVal -> {
+      Assertions.assertEquals(0, callCounter.getAndIncrement());
+      Assertions.assertEquals(Arrays.asList(1, 2, 3, 4), pVal);
+    }).dispose();
+  }
+
+  @Test
+  void test_combineOptionalToList()
+  {
+    Observable<List<Integer>> observableList = Stream.of(Observable.just(Optional.of(1)), Observable.just(Optional.of(2)),
+                                                         Observable.just(Optional.of(3)), Observable.just(Optional.of(4)))
+        .collect(ObservableCollectors.combineOptionalsToList());
+
+    AtomicInteger callCounter = new AtomicInteger(0);
+    observableList.subscribe(pVal -> {
+      Assertions.assertEquals(0, callCounter.getAndIncrement());
+      Assertions.assertEquals(Arrays.asList(1, 2, 3, 4), pVal);
+    }).dispose();
+  }
+
+  @Test
+  void test_combineListToList()
+  {
+    Observable<List<Integer>> observableList = Stream.of(Observable.just(Arrays.asList(1, 2)), Observable.just(Collections.singletonList(2)),
+                                                         Observable.just(Collections.singletonList(3)), Observable.just(Collections.singletonList(4)))
+        .collect(ObservableCollectors.combineListToList());
+
+    AtomicInteger callCounter = new AtomicInteger(0);
+    observableList.subscribe(pVal -> {
+      Assertions.assertEquals(0, callCounter.getAndIncrement());
+      Assertions.assertEquals(Arrays.asList(1, 2, 2, 3, 4), pVal);
+    }).dispose();
+  }
+
+}
