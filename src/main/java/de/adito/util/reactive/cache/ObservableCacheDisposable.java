f8aec17b7a7d1a6afc10fd5506f80e5bc68c819c
Werner Glanzer <w.glanzer@adito.de>
2019-12-26 10:37:39 +0100

    Added ObservableCacheDisposable to dispose ObservableCaches in rxJava way correctly

---
+++ src/main/java/de/adito/util/reactive/cache/ObservableCacheDisposable.java
@@ -0,0 +1,45 @@
+package de.adito.util.reactive.cache;
+
+import io.reactivex.disposables.Disposable;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Disposable to be able to dispose an ObservableCache.
+ * It holds a weak reference to the underlying cache.
+ *
+ * @see ObservableCache
+ * @author w.glanzer, 26.12.2019
+ */
+public class ObservableCacheDisposable implements Disposable
+{
+
+  private WeakReference<ObservableCache> cacheRef;
+
+  public ObservableCacheDisposable(@NotNull ObservableCache pCache)
+  {
+    cacheRef = new WeakReference<>(pCache);
+  }
+
+  @Override
+  public void dispose()
+  {
+    if (isDisposed())
+      return;
+
+    ObservableCache cache = cacheRef.get();
+    if (cache != null)
+    {
+      cacheRef = null;
+      cache.invalidate();
+    }
+  }
+
+  @Override
+  public boolean isDisposed()
+  {
+    return cacheRef == null || cacheRef.get() == null;
+  }
+
+}
