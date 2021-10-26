9d2d9badb9dcb4cd305d823a383d895a3091a222
Werner Glanzer <w.glanzer@adito.de>
2021-10-26 09:23:56 +0200

    feat: added parallelization for ObservableCache's downstream

--- src/main/java/de/adito/util/reactive/cache/ObservableCache.java
+++ src/main/java/de/adito/util/reactive/cache/ObservableCache.java
@@ -6,7 +6,7 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.*;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Function;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.*;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.*;
@@ -50,6 +50,8 @@ public class ObservableCache
       });
   private final Multimap<Object, Disposable> disposableRegistry = Multimaps.synchronizedMultimap(ArrayListMultimap.create());
   private final Cache<Object, CacheValue<?>> cache;
+  private final Scheduler observeScheduler;
+  private final Scheduler subscribeScheduler;
 
   /**
    * Creates an ObservableCache with a maximum number of "unsubscribed" elements.
@@ -61,26 +63,103 @@ public class ObservableCache
   @NotNull
   public static ObservableCache createWithMaxUnsubscribedElements(long pMaxUnsubscribedElements)
   {
+    return createWithMaxUnsubscribedElements(pMaxUnsubscribedElements, null, null);
+  }
+
+  /**
+   * Creates an ObservableCache with a maximum number of "unsubscribed" elements.
+   * All subscribed elements will not be counted.
+   *
+   * @param pMaxUnsubscribedElements maximum number of unsubscribed elements
+   * @param pObserveScheduler        scheduler to be used during observe phase, NULL if you dont want to use a scheduler
+   * @param pSubscribeScheduler      scheduler to be used during subscribe phase, NULL if you dont want to use a scheduler
+   * @return the cache instance
+   */
+  @NotNull
+  public static ObservableCache createWithMaxUnsubscribedElements(long pMaxUnsubscribedElements,
+                                                                  @Nullable Scheduler pObserveScheduler, @Nullable Scheduler pSubscribeScheduler)
+  {
     if (pMaxUnsubscribedElements <= 0)
       throw new IllegalArgumentException("The amount of unsubscribed elements must be larger than 0, because every observable will be " +
                                              "unsubscribed at the beginning.");
 
     return new ObservableCache(CacheBuilder.newBuilder()
                                    .weigher(new CacheValue.SubCountWeigher_IgnoreSubscribedElements())
-                                   .maximumWeight(pMaxUnsubscribedElements));
+                                   .maximumWeight(pMaxUnsubscribedElements), pObserveScheduler, pSubscribeScheduler);
   }
 
   public ObservableCache()
   {
-    //noinspection unchecked,rawtypes
-    this((CacheBuilder) CacheBuilder.newBuilder());
+    this(null, null);
   }
 
-  private ObservableCache(@NotNull CacheBuilder<Object, CacheValue<?>> pCache)
+  public ObservableCache(@Nullable Scheduler pObserveScheduler, @Nullable Scheduler pSubscribeScheduler)
+  {
+    //noinspection unchecked,rawtypes
+    this((CacheBuilder) CacheBuilder.newBuilder(), pObserveScheduler, pSubscribeScheduler);
+  }
+
+  private ObservableCache(@NotNull CacheBuilder<Object, CacheValue<?>> pCache,
+                          @Nullable Scheduler pObserveScheduler, @Nullable Scheduler pSubscribeScheduler)
   {
     cache = pCache
         .removalListener(new _RemovalListener())
         .build();
+    observeScheduler = pObserveScheduler;
+    subscribeScheduler = pSubscribeScheduler;
+  }
+
+  /**
+   * Creates a new Entry in our Observable Cache.
+   * If an Entry with pIdentifier was already created, the previous instance will be returned.
+   * Otherwise the Supplier gets called and a new Observable will be created and shared with a ReplaySubject(!).
+   * The created subject gets parallelized with the observe/subscribe schedulers given in constructor, if available.
+   * So that the cached observable executes its downstream subscriptions will be evaluated in parallel.
+   *
+   * @param pIdentifier Identifier
+   * @param pObservable Observable to cache
+   * @return the cached observable
+   */
+  @NotNull
+  public <T> Observable<T> calculateParallel(@NotNull Object pIdentifier, @NotNull Supplier<Observable<T>> pObservable)
+  {
+    return calculateParallel(pIdentifier, pObservable, observeScheduler, subscribeScheduler);
+  }
+
+  /**
+   * Creates a new Entry in our Observable Cache.
+   * If an Entry with pIdentifier was already created, the previous instance will be returned.
+   * Otherwise the Supplier gets called and a new Observable will be created and shared with a ReplaySubject(!)
+   * The created subject gets parallelized with the observe/subscribe schedulers given in parameters.
+   * So that the cached observable executes its downstream subscriptions will be evaluated in parallel.
+   *
+   * @param pIdentifier         Identifier
+   * @param pObservable         Observable to cache
+   * @param pObserveScheduler   Scheduler that gets used during observe
+   * @param pSubscribeScheduler Scheduler that gets used during subscribe
+   * @return the cached observable
+   */
+  @NotNull
+  public <T> Observable<T> calculateParallel(@NotNull Object pIdentifier, @NotNull Supplier<Observable<T>> pObservable,
+                                             @Nullable Scheduler pObserveScheduler, @Nullable Scheduler pSubscribeScheduler)
+  {
+    return _calculate(pIdentifier, pObservable, pObserveScheduler, pSubscribeScheduler);
+  }
+
+  /**
+   * Creates a new Entry in our Observable Cache.
+   * If an Entry with pIdentifier was already created, the previous instance will be returned.
+   * Otherwise the Supplier gets called and a new Observable will be created and shared with a ReplaySubject(!).
+   * The cached observable will execute its downstream subscriptions in sequence.
+   *
+   * @param pIdentifier Identifier
+   * @param pObservable Observable to cache
+   * @return the cached observable
+   */
+  @NotNull
+  public <T> Observable<T> calculateSequential(@NotNull Object pIdentifier, @NotNull Supplier<Observable<T>> pObservable)
+  {
+    return _calculate(pIdentifier, pObservable, null, null);
   }
 
   /**
@@ -91,22 +170,50 @@ public class ObservableCache
    * @param pIdentifier Identifier
    * @param pObservable Observable to cache
    * @return the cached observable
+   * @deprecated Please decide if you want to use sequential {@link ObservableCache#calculateSequential(Object, Supplier)}
+   * or parallel {@link ObservableCache#calculateParallel(Object, Supplier)} execution
    */
   @NotNull
+  @Deprecated
   public <T> Observable<T> calculate(@NotNull Object pIdentifier, @NotNull Supplier<Observable<T>> pObservable)
+  {
+    return calculateSequential(pIdentifier, pObservable);
+  }
+
+  /**
+   * Creates a new Entry in our Observable Cache.
+   * If an Entry with pIdentifier was already created, the previous instance will be returned.
+   * Otherwise the Supplier gets called and a new Observable will be created and shared with a ReplaySubject(!)
+   *
+   * @param pIdentifier         Identifier
+   * @param pObservable         Observable to cache
+   * @param pObserveScheduler   Scheduler that gets used during observe
+   * @param pSubscribeScheduler Scheduler that gets used during subscribe
+   * @return the cached observable
+   */
+  @NotNull
+  private <T> Observable<T> _calculate(@NotNull Object pIdentifier, @NotNull Supplier<Observable<T>> pObservable,
+                                       @Nullable Scheduler pObserveScheduler, @Nullable Scheduler pSubscribeScheduler)
   {
     try
     {
       //noinspection unchecked We do not have a method to check generic-validity
-      return (Observable<T>) cache.get(pIdentifier, () -> new CacheValue<>(_createErrorHandled(pIdentifier, pObservable)
-                                                                               // serialize it, because AbstractListenerObservables (for example)
-                                                                               // can fire new values async
-                                                                               .serialize()
-                                                                               .replay(1)
-                                                                               .autoConnect(1, pDis -> disposableRegistry.put(pIdentifier, pDis)))
-          // re-evaluate the caches weigh of a value, if the subscription count changes
-          .doOnSubscriptionCountChange(pValue -> cache.put(pIdentifier, pValue)))
-          .getObservable();
+      return (Observable<T>) cache.get(pIdentifier, () -> {
+        Observable<T> observable = _createErrorHandled(pIdentifier, pObservable)
+            .serialize()// serialize it, because AbstractListenerObservables (for example) can fire new values async
+            .replay(1)
+            .autoConnect(1, pDis -> disposableRegistry.put(pIdentifier, pDis));
+
+        if (pObserveScheduler != null)
+          observable = observable.observeOn(pObserveScheduler);
+
+        if (pSubscribeScheduler != null)
+          observable = observable.subscribeOn(pSubscribeScheduler);
+
+        return new CacheValue<>(observable)
+            // re-evaluate the caches weigh of a value, if the subscription count changes
+            .doOnSubscriptionCountChange(pValue -> cache.put(pIdentifier, pValue));
+      }).getObservable();
     }
     catch (ExecutionException e)
     {
