e033d8eb463374e5f37c7eea194f75f3553a8340
Werner Glanzer <w.glanzer@adito.de>
2022-04-13 08:53:06 +0200

    feat: added a cached scheduler to schedule tasks in a cached threadpool

---
+++ src/main/java/de/adito/util/reactive/scheduler/CachedScheduler.java
@@ -0,0 +1,72 @@
+package de.adito.util.reactive.scheduler;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.*;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+/**
+ * This Scheduler is backed up by a cached threadpool.
+ * It will reuse old threads, so that the pool won't grow up too large.
+ *
+ * Difference to Schedulers.computation(): The computation threadpool is a fixed size pool - this one isn't.
+ * Difference to Schedulers.io(): IO pool spawns threads on each event, threads won't get reused - this on does
+ *
+ * @author w.glanzer, 12.04.2022
+ */
+public class CachedScheduler
+{
+
+  private static final String _THREADPOOL_NAME = "tCachedRxScheduler-%d";
+  private static final int _MAX_POOL_SIZE = Integer.MAX_VALUE;
+
+  private static Scheduler _INSTANCE;
+  private static Function<Scheduler, Scheduler> _HANDLER;
+
+  /**
+   * Returns the static instance of this scheduler.
+   * Will be created, if not already existing.
+   *
+   * @see CachedScheduler
+   * @return the scheduler
+   */
+  @NotNull
+  public static Scheduler getInstance()
+  {
+    if(_INSTANCE == null)
+    {
+      ThreadPoolExecutor executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), _MAX_POOL_SIZE, 60, TimeUnit.SECONDS,
+                                                           new LinkedBlockingQueue<>());
+      executor.allowCoreThreadTimeOut(true);
+      executor.setThreadFactory(new ThreadFactoryBuilder()
+                                    .setDaemon(true)
+                                    .setNameFormat(_THREADPOOL_NAME)
+                                    .build());
+
+      Scheduler scheduler = Schedulers.from(executor);
+      if(_HANDLER != null)
+        scheduler = _HANDLER.apply(scheduler);
+
+      _INSTANCE = scheduler;
+    }
+
+    return _INSTANCE;
+  }
+
+  /**
+   * Sets the scheduler handler to modify it on creation
+   *
+   * @param pFn Function that will modify this scheduler
+   */
+  public static void setSchedulerHandler(@NotNull Function<Scheduler, Scheduler> pFn)
+  {
+    if(_INSTANCE == null)
+      Logger.getLogger(CachedScheduler.class.getName()).warning("Setting the scheduler handler after instance creation is currently not supported");
+    _HANDLER = pFn;
+  }
+
+}
---
+++ src/test/java/de/adito/util/reactive/scheduler/Test_CachedScheduler.java
@@ -0,0 +1,54 @@
+package de.adito.util.reactive.scheduler;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.disposables.Disposable;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author w.glanzer, 13.04.2022
+ */
+public class Test_CachedScheduler
+{
+
+  private final Scheduler scheduler = CachedScheduler.getInstance();
+
+  @Test
+  void shouldNotBlockEndless() throws InterruptedException
+  {
+    List<Object> receivedObjects = Collections.synchronizedList(new ArrayList<>());
+
+    // Spam the scheduler to do stuff
+    for (int i = 0; i < 8; i++)
+      scheduler.scheduleDirect(() -> {
+        try
+        {
+          Thread.sleep(500);
+        }
+        catch (Exception e)
+        {
+          // ignore
+        }
+      });
+
+    // Schedule real work
+    for (int i = 0; i < 10; i++)
+      scheduler.scheduleDirect(() -> receivedObjects.add(Observable.create(emitter -> emitter.setDisposable(Disposable.empty()))
+                                                             .startWithItem(123)
+                                                             .observeOn(scheduler)
+                                                             .subscribeOn(scheduler)
+                                                             .blockingFirst()));
+
+    // await all threads ok
+    Thread.sleep(5000);
+
+    // check if all runnable fired
+    assertEquals(10, receivedObjects.size());
+    assertTrue(receivedObjects.stream().allMatch(pObj -> Objects.equals(123, pObj)), receivedObjects.toString());
+  }
+
+}
